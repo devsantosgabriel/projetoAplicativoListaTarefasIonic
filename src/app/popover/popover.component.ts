7c23a8e263d624f9ecfc2bb3a057c0a1e89bae9d
devsantosgabriel <euinstrutorgabriel@gmail.com>
2023-08-28 11:40:24 -0300

    feat> update files again

--- src/app/popover/popover.component.ts
+++ src/app/popover/popover.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-popover',
@@ -8,12 +10,18 @@ import { AuthService } from '../services/auth.service';
 })
 export class PopoverComponent  implements OnInit {
 
-  constructor(public authService : AuthService) { }
+  constructor(public authService : AuthService,
+    private router : Router, 
+    private popOverController : PopoverController) { }
 
   ngOnInit() {}
 
   logout(){
-    this.authService.logout().subscribe()
+    this.authService.logout().subscribe(
+      data => {this.router.navigate(['login'])
+              this.popOverController.dismiss()},
+      err => console.log(err)
+    )
   }
 
 }
